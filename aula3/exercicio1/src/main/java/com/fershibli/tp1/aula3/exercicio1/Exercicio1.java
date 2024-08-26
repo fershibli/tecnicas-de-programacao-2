f9474ffd2feb3a90fbf01e4c4c9396afbad35678
FerShibli <fernandoluiz.1991@gmail.com>
2024-08-26 20:00:31 -0300

    feat(aula3): exercicio1 init

---
+++ aula3/exercicio1/src/main/java/com/fershibli/tp1/aula3/exercicio1/Exercicio1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fershibli.tp1.aula3.exercicio1;
+
+/**
+ *
+ * @author FerShibli
+ */
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
