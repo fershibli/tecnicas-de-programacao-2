ebca7abbfef61c0c4129f244848c4132dbc6f979
FerShibli <fernandoluiz.1991@gmail.com>
2024-09-16 21:54:05 -0300

    feat(aula5): Adicionar campos, getters e setters na classe Cliente

--- aula5/SistemaBancario/src/main/java/com/fershibli/tp1/aula6/sistemabancario/Cliente.java
+++ aula5/SistemaBancario/src/main/java/com/fershibli/tp1/aula6/sistemabancario/Cliente.java
@@ -10,5 +10,154 @@ package com.fershibli.tp1.aula6.sistemabancario;
  */
 public class Cliente {
     private String tableName = "clientes";
+    private int idCli;
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
+    private String email;
+    private String telefone;
+    private String cnpj;
+    private String sexo;
+    private boolean status;
+
+    public Cliente(int idCli, String nome, String cpf, String endereco, String numero, String complemento, String bairro, String cidade, String uf, String cep, String email, String telefone, String cnpj, String sexo, boolean status) {
+        this.idCli = idCli;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.cep = cep;
+        this.email = email;
+    }
+    
+    public int getIdCli() {
+        return idCli;
+    }
+
+    public void setIdCli(int idCli) {
+        this.idCli = idCli;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
     
 }
