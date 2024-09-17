f5f0972cb5781a3b79f48d1cd3b00c4d22b69b7c
FerShibli <fernandoluiz.1991@gmail.com>
2024-09-16 21:53:58 -0300

    feat(aula5): Adicionar campos, getters e setters na classe Agencia

--- aula5/SistemaBancario/src/main/java/com/fershibli/tp1/aula6/sistemabancario/Agencia.java
+++ aula5/SistemaBancario/src/main/java/com/fershibli/tp1/aula6/sistemabancario/Agencia.java
@@ -4,12 +4,116 @@
  */
 package com.fershibli.tp1.aula6.sistemabancario;
 
+
 /**
  *
  * @author Alunos
  */
 public class Agencia {
-    private string tableName = "agencias";
-    private int num_agencia;
+    private String tableName = "agencias";
+    private String numAgencia;
     private String nome;
+    private String endereco;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
+    private String cnpj;
+    private String gerente;
+
+    public Agencia(String numAgencia, String nome, String endereco, String numero, String bairro, String cidade, String uf, String cep, String cnpj, String gerente) {
+        this.numAgencia = numAgencia;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.cep = cep;
+        this.cnpj = cnpj;
+        this.gerente = gerente;
+    }
+
+    public String getNumAgencia() {
+        return numAgencia;
+    }
+
+    public void setNumAgencia(String numAgencia) {
+        this.numAgencia = numAgencia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getGerente() {
+        return gerente;
+    }
+
+    public void setGerente(String gerente) {
+        this.gerente = gerente;
+    }
+    
+    
 }
