98c55ccfd9c8ab500be532615a77524cc4c52ae0
FerShibli <fernandoluiz.1991@gmail.com>
2024-09-16 21:54:13 -0300

    feat(aula5): Adicionar campos, getters e setters na classe ContaCorrente

--- aula5/SistemaBancario/src/main/java/com/fershibli/tp1/aula6/sistemabancario/ContaCorrente.java
+++ aula5/SistemaBancario/src/main/java/com/fershibli/tp1/aula6/sistemabancario/ContaCorrente.java
@@ -10,5 +10,50 @@ package com.fershibli.tp1.aula6.sistemabancario;
  */
 public class ContaCorrente {
     private String tableName = "conta_corrente";
+    private String numConta;
+    private String numAgencia;
+    private int idCli;
+    private int saldo;
+
+    public ContaCorrente(String numConta, String numAgencia, int idCli, int saldo) {
+        this.numConta = numConta;
+        this.numAgencia = numAgencia;
+        this.idCli = idCli;
+        this.saldo = saldo;
+    }
+
+    public String getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(String numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getNumAgencia() {
+        return numAgencia;
+    }
+
+    public void setNumAgencia(String numAgencia) {
+        this.numAgencia = numAgencia;
+    }
+
+    public int getIdCli() {
+        return idCli;
+    }
+
+    public void setIdCli(int idCli) {
+        this.idCli = idCli;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+    
+    
     
 }
