a83970272d7d3dfcd3acfdc3168b5d1e1d77e3e8
FerShibli <fernandoluiz.1991@gmail.com>
2024-10-21 20:57:13 -0300

    Adicionar à classe Usuario validações de cada campo e emitir erros nos setters

--- aula5/SistemaBancario/src/main/java/com/fershibli/tp1/aula6/sistemabancario/Usuario.java
+++ aula5/SistemaBancario/src/main/java/com/fershibli/tp1/aula6/sistemabancario/Usuario.java
@@ -13,9 +13,8 @@ public class Usuario {
     private String login;
     private String senha;
     private int idCli;
-    
+
     public Usuario() {
-        
     }
 
     public Usuario(String login, String senha, int idCli) {
@@ -23,11 +22,23 @@ public class Usuario {
         this.senha = senha;
         this.idCli = idCli;
     }
-    
+
     public void cadastrarTudo(String login, String senha, int idCli) {
         this.login = login;
         this.senha = senha;
         this.idCli = idCli;
+    }
+
+    private boolean validaLogin(String login) {
+        return login != null && login.trim().length() > 3;
+    }
+
+    private boolean validaSenha(String senha) {
+        return senha != null && senha.trim().length() > 5;
+    }
+
+    private boolean validaIdCli(int idCli) {
+        return idCli > 0;
     }
 
     public String getLogin() {
@@ -35,6 +46,9 @@ public class Usuario {
     }
 
     public void setLogin(String login) {
+        if (!validaLogin(login)) {
+            throw new IllegalArgumentException("Login inválido. Deve ter mais de 3 caracteres");
+        }
         this.login = login;
     }
 
@@ -43,6 +57,9 @@ public class Usuario {
     }
 
     public void setSenha(String senha) {
+        if (!validaSenha(senha)) {
+            throw new IllegalArgumentException("Senha inválida. Deve ter mais de 5 caracteres");
+        }
         this.senha = senha;
     }
 
@@ -51,8 +68,9 @@ public class Usuario {
     }
 
     public void setIdCli(int idCli) {
+        if (!validaIdCli(idCli)) {
+            throw new IllegalArgumentException("ID do cliente inválido. Deve ser maior que 0.");
+        }
         this.idCli = idCli;
     }
-    
-    
 }
