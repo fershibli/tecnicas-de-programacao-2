0e812bc5e60ee7462031d1255fe0edabad9abfa1
FerShibli <fernandoluiz.1991@gmail.com>
2024-10-23 21:08:50 -0300

    Adicionar na telaCadastrarUsuario botão para voltar à tela de menu

--- aula5/SistemaBancario/src/main/java/View/TelaCadastrarUsuario.java
+++ aula5/SistemaBancario/src/main/java/View/TelaCadastrarUsuario.java
@@ -42,6 +42,7 @@ public class TelaCadastrarUsuario extends javax.swing.JFrame {
         jToggleButton1 = new javax.swing.JToggleButton();
         senhaUsuario = new javax.swing.JPasswordField();
         confirmaSenha = new javax.swing.JPasswordField();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -81,6 +82,13 @@ public class TelaCadastrarUsuario extends javax.swing.JFrame {
             }
         });
 
+        jButton1.setText("← Voltar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1voltarTelaMenu(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -109,13 +117,18 @@ public class TelaCadastrarUsuario extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(errorLabel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(confirmaSenha))))
+                            .addComponent(confirmaSenha)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(idCliente, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -133,7 +146,7 @@ public class TelaCadastrarUsuario extends javax.swing.JFrame {
                     .addComponent(jLabel4))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(errorLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 133, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 104, Short.MAX_VALUE)
                 .addComponent(jToggleButton1)
                 .addContainerGap())
         );
@@ -173,6 +186,13 @@ public class TelaCadastrarUsuario extends javax.swing.JFrame {
             this.senhasIguais = false;
         }
     }//GEN-LAST:event_verificaSenha
+
+    private void jButton1voltarTelaMenu(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1voltarTelaMenu
+        TelaMenu telaMenu = new TelaMenu();
+        telaMenu.setVisible(true);
+        this.setVisible(false);
+        this.dispose();
+    }//GEN-LAST:event_jButton1voltarTelaMenu
 
     /**
      * @param args the command line arguments
@@ -216,6 +236,7 @@ public class TelaCadastrarUsuario extends javax.swing.JFrame {
     private javax.swing.JPasswordField confirmaSenha;
     private javax.swing.JLabel errorLabel;
     private javax.swing.JTextField idCliente;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
