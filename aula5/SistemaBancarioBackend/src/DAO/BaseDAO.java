ee04d4bc48ba9bd587044c3b47f15dde7094663a
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-04-24 19:57:09 -0300

    Cria a interface BaseDAO com métodos para manipulação de dados em SQL

---
+++ aula5/SistemaBancarioBackend/src/DAO/BaseDAO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package DAO;
+
+import java.util.List;
+
+/**
+ *
+ * @author Alunos
+ */
+public interface BaseDAO {
+    // gets table name
+    public String getTableName();
+    // gets all data from object in SQL format
+    public String dadosSQLValues();
+    // gets all data from object in SQL format to be used in an update statement
+    public String alteraDadosSQLValues();
+    // gets select order by statement in SQL format
+    public String consultaSQLValues();
+    // sets all data from List<String> to object
+    public void importaSQLValues(List<String> dados);
+}
